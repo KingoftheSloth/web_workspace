34db9f4a5d63583ee6fabd6b46044f802431940b
KingoftheSloth <131847767+KingoftheSloth@users.noreply.github.com>
2023-08-04 15:51:31 +0900

    preview jsp

--- jsp1/src/main/java/sample/main/MyMallMain.java
+++ jsp1/src/main/java/sample/main/MyMallMain.java
@@ -7,115 +7,119 @@ import java.util.List;
 import java.util.Scanner;
 
 import sample.dao.JBuyDao;
-import sample.dao.JCustomer;
 import sample.dao.JCustomerDao;
 import sample.dao.JProductDao;
-import sample.dto.JBuy;
-import sample.dto.JProduct;
+import sample.dto.JBuyDto;
+import sample.dto.JCustomerDto;
 import sample.dto.JProductDto;
 import sample.dto.MyPageBuy;
 
 public class MyMallMain {
-	
+
 	public static void main(String[] args) {
-		System.out.println(":::::쇼핑몰에 오신 것을 환영합니다.:::::");
-		System.out.println("<< 상품 소개 >>");
-		JProductDao jProductDao = new JProductDao();
+		System.out.println(":::::: 김땡땡 쇼핑몰에 오신걸 환영합니다. :::::");
+		System.out.println("상품소개");
+		JProductDao jproductDao = new JProductDao();
 		try {
-			List<JProductDto> products = jProductDao.SelectAllProduct();
-			for(JProductDto product : products)
+			List<JProductDto> products = jproductDao.SelectAllProduct();
+			for (JProductDto product : products)
 				System.out.println(product);
-			
 		} catch (SQLException e) {
 			System.out.println("상품소개 예외 : " + e.getMessage());
 		}
-		System.out.println("\n<< 편리한 상품 구매를 위해 검색하기 >>");
+		System.out.println("편리한 상품구매를 위해 검색하기 ");
 		Scanner sc = new Scanner(System.in);
-		System.out.print("검색어 입력 >>> ");
+		System.out.println("검색어 입력 :");
 		String pname = sc.nextLine();
+
 		try {
-			List<JProduct> products = jProductDao.SelectOne(pname);
-			for(JProduct product : products)
+			List<JProductDto> products = JProductDao.SelectOne(pname);
+			for (JProductDto product : products)
 				System.out.println(product);
-			
 		} catch (SQLException e) {
-			System.out.println("상품검색 예외 : "  + e.getMessage());
+			System.out.println("상품 검색 예외 : " + e.getMessage());
 		}
-		
+
+		///////
 		JCustomerDao cdao = new JCustomerDao();
-		JCustomer customer = null;
-		boolean isLogin = false;							//로그인 성공 여부 저장
+		JCustomerDto customer = null;
 		String customid = null;
-		System.out.println("\n<< 장바구니 담기와 상품 구매를 위해 로그인 하기 >>");
-		while(!isLogin) {    //!isLogin == isLogin = false 
-			System.out.print("간편 로그인 - 사용자 id입력 (로그인 취소는 0000) >>> ");
+		boolean isLogin = false; // 로그인 성공 여부 저장
+		while (!isLogin) {
+			System.out.println("\n<< 장바구니와 상품 구매를 위해 로그인하기(필수) >>");
+			System.out.println("간편 로그인 - 사용자 id 입력(로그인 취소는 0000)");
 			customid = sc.nextLine();
-			if(customid.equals("0000")) break;				//반복 종료
+			if (customid.equals("0000"))
+				break;
 			try {
-				customer = cdao.selectById(customid);
-				if(customer != null) {
-					System.out.println(customer.getName() + " 고객님 환영합니다!!");
-					isLogin = true;							  //반복 종료
-				}else
-					System.out.println("존재하지 않는 ID 입니다.");
-			} catch (SQLException e) {
-				System.out.println("간편 로그인 예외 : " + e.getMessage());
+				customer = cdao.login(customid);
+				if (customer != null) {
+					System.out.println(customer.getName() + " 고객님 환영합니다.");
+					isLogin = true;
+				} else
+					System.out.println("유효하지 않은 ID " + customid);
+			} catch (Exception e) {
+				System.out.println("간편 로그인 예외 " + e.getMessage());
 			}
 		}
-		
-		//장바구니 담기는 로그인 상태인 경우만 실행하기
-	      //4. 상품 장바구니 담기 - 장바구니 테이블이 없으므로 구매를 원하는 상품을 main에서 List 로 담기
+// 장바구니 담기는 로그인 상태인 경우만 실행하기
+		// 4. 상품 장바구니 담기 - 장바구니 테이블이 없으므로 구매를 원하는 상품을 List 에 담기
+		// 꼭 필요한것은 pcode랑 quantity
 		JBuyDao bdao = new JBuyDao();
-		List<JBuy> carts = new ArrayList<>();
-		if(isLogin) {
-			while(true) {
-				System.out.println("\n장바구니에 담기 합니다. 그만하려면 상품코드 0000 입력하세요.");
-				System.out.println("구매할 상품 코드 입력하세요. >>> ");
+		List<JBuyDto> carts = new ArrayList<>();
+		if (isLogin) {
+			while (true) {
+				System.out.println("\n 장바구니에 담기 합니다. 그만하려면 상품코드 0000 입력하세요");
+				System.out.println("구매할 상품 코드를 입력하세요 >>>");
 				String pcode = sc.nextLine();
-					if(pcode.equals("0000")) break;
-				System.out.println("구매할 수량을 입력하세요. >>> ");
-					int quantity = Integer.parseInt(sc.nextLine());
-				carts.add(new JBuy(0, customid, pcode, quantity, null));
+				if (pcode.equals("0000")) break;
+				System.out.println("구매할 수량을 입력하세요 >>> ");
+				int quantity = Integer.parseInt(sc.nextLine());
+				carts.add(new JBuyDto(0, customid, pcode, quantity, null));
+				System.out.println("장바구니에 담긴 상품을 결제하시겠습니까? (y/Y)");
+				if (sc.nextLine().toLowerCase().equals("y"))
+					break;
+
+			}
+			System.out.println("장바구니 확인 :" + carts);
+			for(JBuyDto b : carts) {
+				System.out.println("pcode :" + b.getPcode() + "\n수량 :" + b.getQuantity());
 				
-				System.out.println("장바구니에 담긴 상품을 결제하시겠습까? Y/y");
-				if(sc.nextLine().toLowerCase().equals("y")) break;
 			}
-			
-			System.out.println("장바구니 확인 : " + carts);
-			System.out.println("마이페이지 - 장바구니");
-			for(JBuy b : carts) {
-				System.out.println("상품코드 : " + b.getPcode() + ", ���� : " + b.getQuantity());
-			}
-			 //dao에서 carts 를 전달받아 list 만큼 반복하는 insert 실행하기
-			int count = bdao.insertMany(carts);				//구매 테이블에 저장하기
-			if(count != 0)
-				System.out.println("\n결제를 완료했습니다.");
-		      //5번에 장바구니 담긴 상품이 j_buy 테이블에 1)정상 저장, 2)잘못된 수량 rollback 까지 되는지 확인
-            
-		      //6. 마이페이지 - 나의 구매 내역 보기. 총 구매 금액을 출력해 주기 ->sql 테스트 해보고 메소드 작성 시도하기
-			System.out.println(":::현재까지 " + customer.getName() + " 회원님의 구매 내역 입니다.:::");
-			System.out.println(String.format("%12s %20s %8s %10s %13s", "구매날짜","상품명","수량","가격","총금액"));
+			int count = bdao.insertMany(carts);
+			if(count!=0)
+				System.out.println("예외발생. 결제를 중단합니다.");
+			// dao에서 carts를 전달받아 여러번 insert하면 됩니다.
+			else
+			System.out.println("\n" + "결제를 완료했습니다. 현재까지" + customer.getName() + "회원님의 구매 내역 입니다.");
+			//6. 나의 구매내역 보기 
+			System.out.println("나의 구매내역을 보시겠습니까");
 			try {
-				List<MyPageBuy> buys = bdao.mypagebuy(customid);
-				DecimalFormat df = new DecimalFormat("###,###,###");
-				for(MyPageBuy b : buys) {
-					System.out.println(String.format("%15s %20s %5d %10d %16d", 
-							b.getBuy_date(),
-							b.getPname(),
-							b.getQuantity(),
-							df.format(b.getPrice()),
-							df.format(b.getTotal())));
-				}
-				long total = bdao.myMoney(customid);
-				System.out.println("\n" +customer.getName() +" 회원님의 총 구매금액 : " + df.format(total));
+//				System.out.println(bdao.mypagebuy(customid));
+				List<MyPageBuy> results = bdao.mypagebuy(customid);
 				
-			}catch (SQLException e) {
-				System.out.println("구매내역 예외 : " + e.getMessage());
+				System.out.println("현재까지 " + customer.getName() + "회원의 구매내역입니다.");
+				for(MyPageBuy my :results) {
+					System.out.print(my.getBuy_date());
+					System.out.println(" "+ my.getPname()+ "\t" +my.getQuantity() +"개");
+				}
+					
+			} catch (SQLException e) {
+				e.printStackTrace();
 			}
-			
-		}else {   //로그인 안 했을 때
+			System.out.println(customer.getName() + "님의 총 구매가격은 >>");
+			JBuyDao s = new JBuyDao();
+			try {
+				System.out.println(s.myMoney(customid)+ "원입니다.");
+				
+				DecimalFormat df = new DecimalFormat("###,###,###");
+				System.out.println(df.format(s.myMoney(customid))); 
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		
+		} else { // 로그인 안했을 때
 			System.out.println("로그인을 취소했습니다. 프로그램을 종료합니다.");
 		}
-		sc.close();		//맨 끝에 작성
 	}
 }
---
+++ jsp1/src/main/webapp/sample/memberList.jsp
@@ -0,0 +1,23 @@
+<%@page import="sample.dto.Member_Dto"%>
+<%@page import="java.util.List"%>
+<%@page import="sample.dao.Member_Dao"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sample JSP</title>
+</head>
+<body>
+<%
+/* 스크립트릿 : 자바 코드가 들어갈 수 있는 기호.  */
+Member_Dao dao = Member_Dao.getMemberDao();
+List<Member_Dto> list = dao.SelectAll();
+
+
+%>
+<h3>회원목록</h3>
+<%= list %><!-- 표현식 : 변수, 수식등 리턴값이 있는 것들.  -->
+</body>
+</html>
